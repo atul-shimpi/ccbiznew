e0fa75d2c84c8ee85f86e106a12b60e6435b1966
Vishal Chinchane <vishal@veblogy.com>
2016-03-04 14:13:56 +0530

    Tinymce Image upload issue resolved

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -2,10 +2,13 @@ class TinymceAssetsController < ApplicationController
 	def create
     # Take upload from params[:file] and store it somehow...
     # Optionally also accept params[:hint] and consume if needed
+    uploader = StaticImagesUploader.new
+
+    image = uploader.store!(params[:file])
     binding.pry
     render json: {
       image: {
-        url: view_context.image_url(image)
+        url: uploader.url
       }
     }, content_type: "text/html"
   end
