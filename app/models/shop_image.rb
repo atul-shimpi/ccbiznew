bde75d66cee8109dbdc76635917d37d53e01f5ba
Vishal Chinchane <vishal@veblogy.com>
2016-03-04 14:15:23 +0530

    Merged

--- app/models/shop_image.rb
+++ app/models/shop_image.rb
@@ -2,6 +2,12 @@ class ShopImage < ActiveRecord::Base
 	mount_uploader :image, AvatarUploader
 	
 	crop_uploaded :image
-	belongs_to :shop
 
+	belongs_to :shop
+	def self.crop(current_image, crop_x, crop_y, crop_w, crop_h)
+		binding.pry 
+		@img = Magick::Image.read(current_image.image_url)
+		binding.pry
+    	@img.crop!(crop_x, crop_y, crop_w, crop_h)
+	end
 end
