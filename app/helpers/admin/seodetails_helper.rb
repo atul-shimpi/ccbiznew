9d49305647e0f156ec34f9da6a082bea5bbc3c53
Vishal Chinchane <vishal@veblogy.com>
2016-03-14 19:19:25 +0530

    Bug fixes

--- app/helpers/admin/seodetails_helper.rb
+++ app/helpers/admin/seodetails_helper.rb
@@ -1,2 +1,10 @@
 module Admin::SeodetailsHelper
+	def adminshop_seodetails_action(seodetails, action)
+		if action == "new"
+			admin_seodetails_path(seodetails)
+		else
+			admin_seodetail_path(seodetails)
+		end
+
+	end
 end
