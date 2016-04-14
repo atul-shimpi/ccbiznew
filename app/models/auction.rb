2a38e53fbc237aed53cba40097a59b590b8a7aa5
Vishal Chinchane <vishal@veblogy.com>
2016-04-14 11:14:25 +0530

    Delete shop issue fixed

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,9 +1,9 @@
 class Auction < ActiveRecord::Base
   belongs_to :shop
-  has_and_belongs_to_many :skill
-  has_many :teams
-  has_many :players
-  has_many :skills
+  has_and_belongs_to_many :skill, :dependent => :delete_all
+  has_many :teams, :dependent => :delete_all
+  has_many :players, :dependent => :delete_all
+  has_many :skills, :dependent => :delete_all
   is_impressionable
   validates :shop_id, uniqueness: { message: "One Business Website can have only one Auction" }
 end
