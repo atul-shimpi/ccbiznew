3111b22c9c5767d710a5d81262c955dfd863824e
Vishal Chinchane <vishal@veblogy.com>
2016-04-15 15:06:04 +0530

    Admin mail changed

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,7 @@
 class AdminMailer < ApplicationMailer
   def shopcreation_admin_notification_email(shop)
     @shop = shop
-    #mail(:to => "Root.hardik@gmail.com, Root.pankaj@gmail.com", subject: "New Business Website Created - #{shop.name}")
-    mail(:to => "vishal@veblogy.com, chetan.thakkar07@gmail.com", subject: "New Business Website Created - #{shop.name}")
+    mail(:to => "Root.hardik@gmail.com, Root.pankaj@gmail.com", subject: "New Business Website Created - #{shop.name}")
+    #mail(:to => "vishal@veblogy.com, chetan.thakkar07@gmail.com", subject: "New Business Website Created - #{shop.name}")
   end
 end
