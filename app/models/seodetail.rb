4ae00e5dc79029c2e475ed02ebfc43458afdbd14
Vishal Chinchane <vishal@veblogy.com>
2016-10-24 18:52:39 +0530

    Slug udpates

--- app/models/seodetail.rb
+++ app/models/seodetail.rb
@@ -2,5 +2,5 @@ class Seodetail < ActiveRecord::Base
   belongs_to :shop
 
   validates :pagename, uniqueness: { scope: [:shop_id], message: "Details for this page already created" }
-  validates :slug, uniqueness: { message: "Please user different slug" }
+  #validates :slug, uniqueness: { message: "Please user different slug" }
 end
