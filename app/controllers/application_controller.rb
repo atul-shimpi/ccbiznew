29ac53cc97a1ecb3d4ee3e0e1ba33fa143bfc80f
Vishal Chinchane <vishal@veblogy.com>
2016-11-30 20:02:06 +0530

    Domain home page issue resovled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,12 +54,12 @@ protected
   end
   def get_shop    
     subdomain = request.subdomain.split(".").last   
-    if subdomain.blank? || subdomain =='www'  
+    if subdomain.blank? || subdomain =='www'        
       if subdomain.nil?
-        @shop = Shop.find_by_domain('www.'+request.host)  
-      else
         @shop = Shop.find_by_domain(request.host) 
-      end   
+      else
+        @shop = Shop.find_by_domain(request.domain) 
+      end    
       
     else
       @shop = Shop.find_by_subdomain(subdomain) 
